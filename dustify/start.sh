55d5bab78e19dd5bae3038ab9018af83051265f7
Timo Reimann <ttr314@googlemail.com>
2017-02-13 13:55:38 +0100

    Add dust-compatible configration.
    Also add custom start script.

---
+++ dustify/start.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+SRCDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"; readonly SRCDIR
+readonly PROJECTDIR="${SRCDIR}/.."
+readonly CONFIGDIR="${SRCDIR}/configs"
+
+usage() {
+  echo "usage: $(basename "$0") -h | [-b] [-n <Kubernetes namespace>] [-- <Traefik arguments>]
+  -h: this help screen
+  -b: build Traefik prior to running
+  -n <Kubernetes namespace>: restrict to the given namespace [default: 'default']
+" >&2
+exit 1
+}
+
+BUILD=
+KUBE_NAMESPACE=default
+while getopts ":bhn:" opt; do
+  case "${opt}" in
+    b)
+      BUILD=yes
+      ;;
+    h)
+      usage
+      ;;
+    n)
+      KUBE_NAMESPACE="${OPTARG}"
+      ;;
+    --)
+      break
+      ;;
+    \?)
+      echo "invalid option: -$OPTARG" >&2
+      usage
+      ;;
+    :)
+      echo "option -$OPTARG requires an argument" >&2
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND-1))
+readonly BUILD
+readonly KUBE_NAMESPACE
+
+if [[ "${BUILD}" ]]; then
+  echo "building traefik backend..."
+  ( cd "${PROJECTDIR}" && go build -o dist/traefik . )
+fi
+
+"${PROJECTDIR}/dist/traefik" \
+  --configfile="${CONFIGDIR}/traefik.dust.toml" \
+  --marathon.filename="${CONFIGDIR}/marathon.dust.tmpl" \
+  --kubernetes.namespaces="${KUBE_NAMESPACE}" \
+  --kubernetes.filename="${CONFIGDIR}/kubernetes.dust.tmpl" \
+  "$@"
